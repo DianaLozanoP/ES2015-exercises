86f099e5700ac8c9af7ce3d8f2e55412ff72389f
Diana Lozano <127370167+DianaLozanoP@users.noreply.github.com>
2023-06-11 18:32:48 -0400

    Update destructuring-exercise.js

--- destructuring-exercise.js
+++ destructuring-exercise.js
@@ -4,3 +4,95 @@ let {numPlanets, yearNeptuneDiscovered} = facts;
 
 console.log(numPlanets); // 8
 console.log(yearNeptuneDiscovered); // 1846
+//What does the following code return/print?
+let planetFacts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+  };
+  
+  let {numPlanets, ...discoveryYears} = planetFacts;
+  
+  console.log(discoveryYears); // 1846, 1659
+
+//What does the following code return/print?
+function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // 'Your name is Alejandro and you like pruple'
+  getUserData({firstName: "Melissa"}) // 'Your name is Melissa and you like green.
+  getUserData({}) // 'Your name is undefined and you like green'
+  
+//What does the following code return/print?
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // 'Maya'
+console.log(second); // 'Marisa'
+console.log(third); // 'Chi'
+
+//What does the following code return/print?
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); // 'Raindrops on roses'
+  console.log(whiskers); // 'whiskers on kittens'
+  console.log(aFewOfMyFavoriteThings); // ["Bright copper kettles", "warm woolen mittens","Brown paper packages tied up with strings"]
+
+//What does the following code return/print?
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // [20,10,30]
+
+//## **ES2015 Refactoring**
+// In this exercise, you’ll refactor some ES5 code into ES2015.
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  var a = obj.numbers.a;
+  var b = obj.numbers.b;
+/* Write an ES2015 Version */
+const {numbers : {a,b}}= obj
+
+//ES5 Array Swap
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+//## **Object Destructuring 1**
+[1,2]=[2,1]
+
+
+// ## **raceResults()**
+
+// Write a function called ***raceResults*** which accepts a single array argument. It should return an object with the keys ***first***, ***second***, ***third***, and ***rest***.
+
+// - first: the first element in the array
+// - second: the second element in the array
+// - third: the third element in the array
+// - rest: all other elements in the array
+
+// **Write a *one line* function to make this work using**
+
+// - An arrow function
+// - Destructuring
+// - `Enhanced` object assignment (same key/value shortcut)
+
+function raceResults(first, second, third, ...rest){
+    return {
+        [first]: first,
+        [second]: second,
+        [third]: third,
+        [rest]: rest
+    }
+}
